9e004981ecf5c541fe5e1c84f671815853d2ef89
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-09-25 11:46:24 +0800

    完善判断逻辑

--- js/drop.js
+++ js/drop.js
@@ -8,12 +8,12 @@ function handleDrop(e) {
     // 获取文件名和后缀名并进行处理
     var fileName = file.name;
     var fileExtension = fileName.split('.').pop();
-    
+
     if (fileExtension.toLowerCase() === 'xml') {
-        // 对xml文件进行处理
-    } else {
-        // 文件后缀名不是xml，进行其他处理
-        // 这里可以显示一个错误提示或者执行其他操作
+        showLoadBtn(); //显示转换中
+    }
+    else {
+        typeError();
     }
 }
 
@@ -21,28 +21,37 @@ function handleDragOver(e) {
     e.preventDefault();
 }
 
-function startHide(){
+function startHide() {
     hideSuccessBadge();
     hideDangerBadges();
     hideInfoBadge();
 }
 
-function hideInfoBadge(){
+function hideInfoBadge() {
     var infoBadge = $('.badge-info'); // 获取元素
     infoBadge.hide(); // 隐藏
 }
 
-function hideSuccessBadge(){
+function hideSuccessBadge() {
     var successBadge = $('.badge-success'); // 获取元素
     successBadge.hide(); // 隐藏
 }
 
-function hideDangerBadges(){
+function hideDangerBadges() {
     var dangerBadges = $('.badge-danger'); // 获取元素
     dangerBadges.hide(); // 隐藏
 }
 
-function showLoadBtn(){
+function showLoadBtn() {
     var loadBtn = $('.btn-primary'); // 获取元素
-    loadBtn.show(); // 隐藏
+    loadBtn.show(); // 显示
+}
+
+function typeError() {
+    // 获取元素
+    var typeError = $('.type-error');
+    var loadBtn = $('.btn-primary');
+    
+    typeError.show();
+    loadBtn.hide();
 }
